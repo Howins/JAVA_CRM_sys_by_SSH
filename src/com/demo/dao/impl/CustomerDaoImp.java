b2ddaf741dc38ee58f312048be8d4ac1347d15f0
Howins <sinkyoho@163.com>
2020-06-03 09:52:21 +0800

    base structure built

---
+++ src/com/demo/dao/impl/CustomerDaoImp.java
@@ -0,0 +1,7 @@
+package com.demo.dao.impl;
+
+import com.demo.dao.CustomerDao;
+
+public class CustomerDaoImp implements CustomerDao {
+
+}
---
+++ src/com/demo/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,7 @@
+package com.demo.service.impl;
+
+import com.demo.service.CustomService;
+
+public class CustomerServiceImpl implements CustomService{
+
+}
